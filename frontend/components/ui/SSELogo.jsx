81205a04404ca7281630a33a26d30d0474f889ca
Naga Mohan Madicharla <163634617+unknownsteve7@users.noreply.github.com>
2025-08-06 00:35:23 +0530

    Delete frontend directory

--- frontend/components/ui/SSELogo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SSELogo = ({ width = 200, height = 90, className = "" }) => {
-    return (
-       <img src="sgi.png" alt="sgi" width={width} height={height} className={className} />
-    );
-};
-
-export default SSELogo;
