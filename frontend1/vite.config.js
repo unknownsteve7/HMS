87e9d80b280d9566fb551034ca9690ef86fd49e7
Madicharla Naga Mohan <163634617+unknownsteve7@users.noreply.github.com>
2025-11-30 14:21:50 +0530

    Add Tailwind CSS plugin to Vite configuration

--- frontend1/vite.config.js
+++ frontend1/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
   plugins: [
+    tailwindcss(),
     react(),
     {
       name: 'spa-fallback',
